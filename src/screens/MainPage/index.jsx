a2635cb97e64185819ab1210d0b8e82e9757fbd2
Milton <miltonsergiom@gmail.com>
2022-05-07 00:06:17 -0300

    Second code refactoring update

--- src/screens/MainPage/index.jsx
+++ src/screens/MainPage/index.jsx
@@ -18,38 +18,16 @@ export function MainPage() {
       const cities = ["Nuuk", "Urubici", "Nairobi"];
       const time = new Date().toLocaleTimeString("en-US");
 
-      await api
-        .get(`/weather?q=${cities[0]}&appid=${key}&units=metric`)
-        .then((res) => {
-          setData(prevState => ({ ...prevState, city1: res.data }));
-          localStorage.removeItem(`@challenge-wether:${cities[0]}`);
+      cities.map((city) => {
+        api.get(`/weather?q=${city}&appid=${key}&units=metric`).then((res) => {
+          setData((prevState) => ({ ...prevState, [city]: res.data }));
+          localStorage.removeItem(`@challenge-wether:${city}}`);
           localStorage.setItem(
-            `@challenge-wether:${cities[0]}`,
+            `@challenge-wether:${city}`,
             JSON.stringify(res.data)
           );
         });
-
-      await api
-        .get(`/weather?q=${cities[1]}&appid=${key}&units=metric`)
-        .then((res) => {
-          setData(prevState => ({ ...prevState, city2: res.data }));
-          localStorage.removeItem(`@challenge-wether:${cities[1]}`);
-          localStorage.setItem(
-            `@challenge-wether:${cities[1]}`,
-            JSON.stringify(res.data)
-          );
-        });
-
-      await api
-        .get(`/weather?q=${cities[2]}&appid=${key}&units=metric`)
-        .then((res) => {
-          setData(prevState => ({ ...prevState, city3: res.data }));
-          localStorage.removeItem(`@challenge-wether:${cities[2]}`);
-          localStorage.setItem(
-            `@challenge-wether:${cities[2]}`,
-            JSON.stringify(res.data)
-          );
-        });
+      });
 
       setLastUpdate(time);
     }
@@ -58,7 +36,7 @@ export function MainPage() {
     // setInterval(getData, 600000);
   }, []);
 
-  console.log("Resposta setada", data);
+  // console.log("Resposta setada", data);
 
   return (
     <div className="container">
@@ -68,9 +46,11 @@ export function MainPage() {
       <div className="content-container">
         <div className="cards-container">
           {data &&
-          Object.values(data).map(card => 
-              <InfoCard info={card} updatedAt={lastUpdate} />
-            )}
+            Object.values(data).map((card) => (
+              <div key={card.id}>
+                <InfoCard info={card} updatedAt={lastUpdate} />
+              </div>
+            ))}
         </div>
       </div>
     </div>
